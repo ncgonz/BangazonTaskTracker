c1c6980f2a319cc37806f4ff5eaa450f4c9698cc
Sandy Jones <sandynashv@gmail.com>
2017-01-18 16:31:13 -0600

    added DAL

---
+++ BangazonTaskTracker/Controllers/UserTaskController.cs
@@ -0,0 +1,45 @@
+﻿using BangazonTaskTracker.DAL;
+using BangazonTaskTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BangazonTaskTracker.Controllers
+{
+    public class UserTaskController : ApiController
+    {
+        UserTaskRepository Repo = new UserTaskRepository();
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public UserTask Get(int id)
+        {
+            return Repo.GetUserTaskById(id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]UserTask _userTask)
+        {
+            Repo.AddUserTask(_userTask);
+            //this is not completed left here only for build purposes
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- BangazonTaskTracker/DAL/UserTaskRepository.cs
+++ BangazonTaskTracker/DAL/UserTaskRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BangazonTaskTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,8 +8,37 @@ namespace BangazonTaskTracker.DAL
 {
     public class UserTaskRepository
     {
-        UserTaskContext Context = new UserTaskContext();
-        UserTaskRepository Repo = new UserTaskRepository();
+        UserTaskContext Context { get; set; }
+        public UserTaskRepository()
+        {
+            Context = new UserTaskContext();
+        }
+        public UserTaskRepository(UserTaskContext _context)
+        {
+            Context = _context;
+        }
+        public void AddUserTask(UserTask _userTask)
+        {
+            Context.UserTasks.Add(_userTask);
+            Context.SaveChanges();
+        }
+        public void RemoveUserTask(UserTask _userTask)
+        {
+            Context.UserTasks.Remove(_userTask);
+            Context.SaveChanges();
+        }
+        public UserTask GetUserTaskById(int userTaskId)
+        {
+            UserTask found_userTask = Context.UserTasks.FirstOrDefault(i => i.Id == userTaskId);
+            if (found_userTask != null)
+            {
+                return found_userTask;
+            }
+            else
+            {
+                return null;
+            }
 
+        }
     }
 }
