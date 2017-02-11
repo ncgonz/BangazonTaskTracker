340e3317981ca89a866491a65e397fe8b489b197
Sandy Jones <sandynashv@gmail.com>
2017-02-10 21:26:28 -0600

    added UserTaskController to app file

--- BangazonTaskTracker/Scripts/app/app.js
+++ BangazonTaskTracker/Scripts/app/app.js
@@ -1,3 +1,13 @@
-﻿var app = angular.module("Tracker", [], function () {
+﻿var app = angular.module("Tracker", ["ngRoute"]);
 
-});
+
+app.controller('UserTaskController', ['$scope', '$http', function ($scope, $http) {
+    $http({
+        url: '/api/UserTaskController/',
+        method: "GET"
+    })
+        .then(function (result) {
+            $scope.UserTasks = result.data;
+        }, function (error) {
+        });
+}]);
