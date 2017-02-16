aeedb2112e41d6f391aaf7310354259a3e73a545
Sandy Jones <sandynashv@gmail.com>
2017-02-15 20:59:43 -0600

    added scripts

--- BangazonTaskTracker/Scripts/app/app.js
+++ BangazonTaskTracker/Scripts/app/app.js
@@ -1,13 +1,15 @@
 ﻿var app = angular.module("Tracker", ["ngRoute"]);
 
+//put my app.config angular stuff here
 
 app.controller('UserTaskController', ['$scope', '$http', function ($scope, $http) {
-    $http({
-        url: '/api/UserTaskController/',
-        method: "GET"
-    })
+        $http({
+            url: '/api/UserTask',
+            method: "GET"
+        })
         .then(function (result) {
             $scope.UserTasks = result.data;
         }, function (error) {
-        });
+        })
 }]);
+
