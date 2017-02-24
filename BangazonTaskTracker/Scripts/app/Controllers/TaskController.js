ccbf3a38742c74767302cbcb1f1ccfc2a9b1d4af
Sandy Jones <sandynashv@gmail.com>
2017-02-23 21:05:21 -0600

    TaskFactory and TaskController

---
+++ BangazonTaskTracker/Scripts/app/Controllers/TaskController.js
@@ -0,0 +1,44 @@
+﻿"use strict";
+
+app.controller('TaskController', ['$scope', '$location', '$TaskFactory', '$http', function ($scope, $location, TaskFactory, $http) {
+    $scope.getATask;
+    $scope.getATask = function () {
+        TaskFactory.getList($scope.getATask).then(function (taskReturn) {
+            $scope.taskArray = taskReturn.dataset.data[0];
+        })
+    }
+}]);
+    
+
+
+    app.controller('EditTaskController', ['$scope', '$http', function ($scope, $http) {
+        $http({
+            url: '/api/UserTask',
+            method: "PUT"
+        })
+        .then(function (result) {
+            $scope.UserTask = result.data;
+        }, function (error) {
+        })
+    }]);
+    app.controller('DeleteTaskController', ['$scope', '$http', function ($scope, $http) {
+        $http({
+            url: '/api/UserTask',
+            method: "DELETE"
+        })
+        .then(function (result) {
+            $scope.UserTask = result.data;
+        }, function (error) {
+        })
+    }]);
+    app.controller('AddTaskController', ['$scope', '$http', function ($scope, $http) {
+        $http({
+            url: '/api/UserTask',
+            method: "POST"
+        })
+        .then(function (result) {
+            $scope.UserTask = result.data;
+        }, function (error) {
+        })
+    }]);
+
---
+++ BangazonTaskTracker/Scripts/app/Factories/TaskFactory.js
@@ -0,0 +1,13 @@
+﻿"use strict"
+app.factory("TaskFactory", function ($q, $http) {
+
+    var getList = function () {
+        return $q(function (resolve, reject) {
+            $http.get('~/api/UserTask')
+        }).success(function (taskListing) {
+            resolve(taskListing)
+        }, function (error) {
+            reject(error);
+        });
+    };
+});
