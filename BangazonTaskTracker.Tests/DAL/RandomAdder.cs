f647ba814cb72e285eb6b80e0ce15b8b2876a9fd
Nathan Gonzalez <ncgonz@gmail.com>
2017-03-08 19:06:33 -0600

    oops, forgot a file... :(

---
+++ BangazonTaskTracker.Tests/DAL/RandomAdder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BangazonTaskTracker.Tests.DAL
+{
+    public class RandomAdder
+    {
+        readonly RandomNumberGenerator _randomNumberGenerator;
+
+        public RandomAdder(RandomNumberGenerator randomNumberGenerator)
+        {
+            _randomNumberGenerator = randomNumberGenerator;
+        }
+
+        public int AddRandomly(int addToRandomNumber)
+        {
+            return addToRandomNumber + _randomNumberGenerator.RandomNumberToAdd;
+        }
+    }
+
+    public class RandomNumberGenerator
+    {
+        public virtual int RandomNumberToAdd { get; set; }
+
+        public RandomNumberGenerator()
+        {
+            Random rnd = new Random();
+            RandomNumberToAdd = rnd.Next(1, 13);
+        }
+    }
+
+    class MockedRandomNumberGenerator : RandomNumberGenerator
+    {
+        public override int RandomNumberToAdd { get { return 2; } set {Console.WriteLine(value);} }
+    }
+}
